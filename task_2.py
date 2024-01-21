70220e93f67b8204bee4eb4091d31a0e6336bda5
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2024-01-21 13:04:25 +0100

    fixed the snowflake

--- task_2.py
+++ task_2.py
@@ -8,22 +8,30 @@ def koch_snowflake(t, order, size):
             koch_snowflake(t, order - 1, size / 3)
             t.left(angle)
 
+def draw_full_snowflake(t, order, size):
+    for _ in range(3):
+        koch_snowflake(t, order, size)
+        t.right(120)
+
 def main():
-    recursion_level = int(input("Введіть рівень рекурсії для сніжинки Коха: "))
+    recursion_level = int(input("Enter the recursion level for the Koch snowflake: "))
 
     screen = turtle.Screen()
     screen.bgcolor("white")
-    screen.title("Сніжинка Коха")
+    screen.title("Koch Snowflake")
 
     snowflake_turtle = turtle.Turtle()
     snowflake_turtle.speed(0)
-
     snowflake_turtle.penup()
     snowflake_turtle.goto(-150, 90)
     snowflake_turtle.pendown()
     snowflake_turtle.left(60)
 
-    koch_snowflake(snowflake_turtle, recursion_level, 300)
+    screen.tracer(0)
+
+    draw_full_snowflake(snowflake_turtle, recursion_level, 300)
+
+    screen.update()
 
     screen.exitonclick()
 
