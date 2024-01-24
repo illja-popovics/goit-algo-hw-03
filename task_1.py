5865a5a1b4dccdb4207255f5f2266bbbb5b0dff4
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2024-01-24 09:47:39 +0100

    made recursive

--- task_1.py
+++ task_1.py
@@ -1,26 +1,29 @@
-import os
 import shutil
 import argparse
+from pathlib import Path
 
-def recursive_copy(src_dir, dest_dir):
+def recursive_copy(src_path, dest_dir):
     try:
-        os.makedirs(dest_dir, exist_ok=True)
+        src_path = Path(src_path)
+        dest_dir = Path(dest_dir)
+        dest_dir.mkdir(parents=True, exist_ok=True)
 
-        for root, _, files in os.walk(src_dir):
-            for file in files:
-                src_path = os.path.join(root, file)
-                copy_file_to_sorted_dir(src_path, dest_dir)
+        for item in src_path.iterdir():
+            if item.is_dir():
+                recursive_copy(item, dest_dir)
+            elif item.is_file():
+                copy_file_to_sorted_dir(item, dest_dir)
 
     except Exception as e:
         print(f"Error: {e}")
 
 def copy_file_to_sorted_dir(src_file, dest_dir):
     try:
-        extension = os.path.splitext(src_file)[1][1:]  # Get file extension without dot
-        dest_subdir = os.path.join(dest_dir, extension)
+        extension = src_file.suffix[1:]  # Get file extension without dot
+        dest_subdir = dest_dir / extension
 
-        os.makedirs(dest_subdir, exist_ok=True)
-        dest_file = os.path.join(dest_subdir, os.path.basename(src_file))
+        dest_subdir.mkdir(parents=True, exist_ok=True)
+        dest_file = dest_subdir / src_file.name
         shutil.copy2(src_file, dest_file)
 
     except Exception as e:
@@ -28,14 +31,14 @@ def copy_file_to_sorted_dir(src_file, dest_dir):
 
 def main():
     parser = argparse.ArgumentParser(description="Recursive file copy and sorting")
-    parser.add_argument("src_dir", help="Source directory path")
+    parser.add_argument("src_path", help="Source path")
     parser.add_argument("dest_dir", nargs="?", default="dist", help="Destination directory path (default: 'dist')")
     args = parser.parse_args()
 
-    src_dir = os.path.abspath(args.src_dir)
-    dest_dir = os.path.abspath(args.dest_dir)
+    src_path = args.src_path
+    dest_dir = args.dest_dir
 
-    recursive_copy(src_dir, dest_dir)
+    recursive_copy(src_path, dest_dir)
     print("Files copied and sorted successfully.")
 
 if __name__ == "__main__":
